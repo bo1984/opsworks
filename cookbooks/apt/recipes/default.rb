b0859b398b6c27acbcbb5c0923310d447f9638a8
bo1984 <bolechangeur@gmail.com>
2016-06-02 17:55:01 +0000

    First recipes made commit

---
+++ cookbooks/apt/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: apt
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "yum update" do
+  command "yum update"
+end
---
+++ cookbooks/metadata.rb
@@ -0,0 +1,4 @@
+name	"test"
+maintainer	"Bo Lechangeur
+maintainer_email	"bolechangeur@gmail.com"
+
